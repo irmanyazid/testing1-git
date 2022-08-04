b419a74ec05eb03cdc7f5768052edcc39010118e
Irman Yazid <irmandizay@gmail.com>
2022-08-04 11:24:15 +0700

    Add file test.dart

---
+++ lib/test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TestingTesting extends StatelessWidget {
+  const TestingTesting({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Text('Testing 1 Git'),
+      ),
+    );
+  }
+}
